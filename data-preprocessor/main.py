c9799d032daa80d32645039d4e6bd1dca701ce32
CosmicGenos <nimesha12417@usci.ruh.ac.lk>
2026-08-16 21:07:26 +0530

    added sence splitter

--- data-preprocessor/main.py
+++ data-preprocessor/main.py
@@ -3,6 +3,8 @@ import numpy as np
 import ffmpeg
 from pathlib import Path
 import sys
+from scenedetect import detect , ContentDetector , split_video_ffmpeg
+import uuid
 
 
 parser = argparse.ArgumentParser(description="Data Preprocessor") #create parser and use that to parse arguments
@@ -28,6 +30,49 @@ def change_video_resalution(args: argparse.Namespace):
     )
 
 
+def scene_splitter(args:argparse.Namespace):
+    probe = ffmpeg.probe(args.input_path)
+    video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+
+    if video_stream is None:
+        raise ValueError("No Video Stream Found ")
+
+    scenes = detect(str(args.input_path),ContentDetector())
+
+    data = args.output_path
+
+    videofolder: Path = data / f"{args.input_path.name}_{uuid.uuid4()}"
+    videofolder.mkdir(parents=True, exist_ok=True)
+
+    split_video_ffmpeg(
+        args.input_path,
+        scenes,
+        output_dir=videofolder,
+        show_progress = True
+    )
+
+    file_names = list(videofolder.iterdir())
+
+    less_tha_3s_videos = [f for f in file_names if float(ffmpeg.probe( str(f))["format"].get("duration",0)) < 3.0 ]
+
+    for f in less_tha_3s_videos:
+        (f).unlink(missing_ok = True)
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
 def create_parser() -> argparse.ArgumentParser:
 
@@ -50,6 +95,14 @@ def create_parser() -> argparse.ArgumentParser:
     convert.add_argument("output_path", type=Path ,help = "output path for video")
 
     convert.set_defaults(func = change_video_resalution )
+
+    spiltv = subparsers.add_parser("spiltv",help="will split the video")
+
+    spiltv.add_argument("input_path", type=Path ,help = "input path for video")
+
+    spiltv.add_argument("output_path", type=Path ,help = "output path for video")
+
+    spiltv.set_defaults(func = scene_splitter )
 
     return parser
 
